770413471dcf923cc010dabfaac44949d2548506
fattaah99 <fattaah99@gmail.com>
2025-01-07 16:38:04 +0700

    upload laporan

--- application/views/Buku/create.php
+++ application/views/Buku/create.php
@@ -64,7 +64,7 @@
                             Foto</label>
                         <input
                             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            aria-describedby="foto_help" id="foto" type="file" name="foto" required>
+                            aria-describedby="foto_help" id="foto" type="file" name="foto">
                         <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="foto_help">Unggah foto buku
                         </div>
                     </div>
